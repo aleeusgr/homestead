6b0443adfa40739754c63af6bbb8a95d7cc35f33
Alex <alexeusgr@gmail.com>
2023-12-21 13:32:39 +0200

    gitignore and 1.0 UI

--- flake.nix
+++ flake.nix
@@ -3,6 +3,16 @@
 
   outputs = { self, nixpkgs }: {
 
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.my-script;
+
+    packages.x86_64-linux.my-script =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+      pkgs.writeShellScriptBin "my-script" ''
+        DATE="$(${pkgs.ddate}/bin/ddate +'the %e of %B%, %Y')"
+        ${pkgs.cowsay}/bin/cowsay Today is $DATE. You are standing west of house. There is a note at the wall.
+      '';
     nixosConfigurations.container = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =

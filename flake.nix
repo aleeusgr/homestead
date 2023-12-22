c5ce98531c394bd88beb00cdf99fe4b5087f1c34
Alex <alexeusgr@gmail.com>
2023-12-22 16:37:51 +0200

    add scaffold for building C

--- flake.nix
+++ flake.nix
@@ -1,50 +1,33 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
 
-  outputs = { self, nixpkgs }: {
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
-    packages.x86_64-linux.view_note =
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in
-      pkgs.writeShellScriptBin "view_note" ''
-        ${pkgs.cowsay}/bin/cowsay Note says: help, who, what, where.
-      '';
-
-    packages.x86_64-linux.hello =
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in
-      pkgs.writeShellScriptBin "hello" ''
+    in rec {
+      defaultApp = flake-utils.lib.mkApp {
+        drv = defaultPackage;
+      };
+      hello = pkgs.writeShellScriptBin "hello" ''
         DATE="$(${pkgs.ddate}/bin/ddate +'the %e of %B%, %Y')"
         ${pkgs.cowsay}/bin/cowsay Today is $DATE. You are standing west of house. There is a note at the wall.
       '';
 
-    # #5 and #7, lets go to the moon and get it running on an embedded device.  
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules =
-        [ ({ pkgs, ... }: {
-            boot.isContainer = true;
-
-            # Let 'nixos-version --json' know about the Git revision
-            # of this flake.
-            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-
-            # Network configuration.
-            networking.useDHCP = false;
-            networking.firewall.allowedTCPPorts = [ 80 ];
-
-            # Enable a web server.
-            services.httpd = {
-              enable = true;
-              adminAddr = "alexeusgr@gmail.com";
-            };
-          })
-        ];
-    };
-
-  };
+      defaultPackage = hello;
+      # devShell = pkgs.mkShell {
+      #   buildInputs = [
+      #     lightgbm-cli
+      #   ];
+      # };
+    }
+  );
 }
